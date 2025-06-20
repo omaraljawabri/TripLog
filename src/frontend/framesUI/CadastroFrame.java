e7939a976bf8b06009124b4a2b7b806b99712e49
Omar Al Jawabri <164200540+omaraljawabri@users.noreply.github.com>
2025-06-20 18:50:56 -0300

    Correção path da imagem

--- src/frontend/framesUI/CadastroFrame.java
+++ src/frontend/framesUI/CadastroFrame.java
@@ -147,7 +147,7 @@ public class CadastroFrame {
         imagePanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
         try {
-            ImageIcon originalIcon = new ImageIcon(getClass().getResource("/resources/images/viagem_site.png"));
+            ImageIcon originalIcon = new ImageIcon(getClass().getResource("../resources/images/viagem_site.png"));
             Image scaledImage = originalIcon.getImage().getScaledInstance(500, 500, Image.SCALE_SMOOTH);
             ImageIcon scaledIcon = new ImageIcon(scaledImage);
 
