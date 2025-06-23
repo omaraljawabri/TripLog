1d55857777db5d1464441f7b507878f0f5b4ce6f
Omar Al Jawabri <164200540+omaraljawabri@users.noreply.github.com>
2025-06-23 11:46:52 -0300

    Correcao no nome das exceptions

--- src/backend/main/services/AuthService.java
+++ src/backend/main/services/AuthService.java
@@ -1,7 +1,8 @@
 package backend.main.services;
 
 import backend.main.entities.Viajante;
-import backend.main.exceptions.ValidationException;
+import backend.main.exceptions.ErroInternoException;
+import backend.main.exceptions.ValidacaoException;
 import backend.main.repositories.ViajanteRepository;
 import backend.main.utils.SenhaUtil;
 
@@ -16,21 +17,27 @@ public class AuthService {
 
     public void cadastrar(Viajante viajante){
         if (viajante.getEmail() == null || viajante.getNome() == null || viajante.getSenha() == null){
-            throw new ValidationException("Email, nome e senha devem ser preenchidos");
+            throw new ValidacaoException("Email, nome e senha devem ser preenchidos");
         }
+
+        Viajante viajantePorEmail = viajanteRepository.buscarViajantePorEmail(viajante.getEmail());
+        if (viajantePorEmail != null){
+            throw new ValidacaoException("Email informado já está em uso");
+        }
+
         String senhaCodificada = SenhaUtil.hashSenha(viajante.getSenha());
         viajante.setSenha(senhaCodificada);
         boolean resultado = viajanteRepository.salvarViajante(viajante);
 
         if (!resultado){
-            throw new RuntimeException("Erro ao fazer cadastro");
+            throw new ErroInternoException("Erro ao fazer cadastro");
         }
     }
 
     public Viajante login(String senha, String email){
         Viajante viajanteBuscado = viajanteRepository.buscarViajantePorEmail(email);
         if (viajanteBuscado == null || !SenhaUtil.verificarSenha(senha, viajanteBuscado.getSenha())){
-            throw new ValidationException("Viajante com email ou senha incorreto(s)!");
+            throw new ValidacaoException("Viajante com email ou senha incorreto(s)!");
         }
         return viajanteBuscado;
     }
